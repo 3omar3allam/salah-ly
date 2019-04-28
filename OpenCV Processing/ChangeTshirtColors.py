a875ad5698670f3b8a26415342fd7e17fafcb504
amrabdelhamidhassan <33031176+amrabdelhamidhassan@users.noreply.github.com>
2019-04-28 15:23:00 +0200

    Update ChangeTshirtColors.py

--- OpenCV Processing/ChangeTshirtColors.py
+++ OpenCV Processing/ChangeTshirtColors.py
@@ -12,6 +12,12 @@ def ChangeTshirtColors (img, lower_color_bounds1, upper_color_bounds1, transferC
     # converting img to HSV
     frame = img
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    Red1=False 
+    if lower_color_bounds1[0]>upper_color_bounds1[0] :
+        Red1=True
+    Red2=False    
+    if lower_color_bounds2[0]>upper_color_bounds2[0] :
+        Red2=True
 
     # Creating Transformation Matrices
     image_h, image_w, image_c = frame.shape
@@ -19,7 +25,7 @@ def ChangeTshirtColors (img, lower_color_bounds1, upper_color_bounds1, transferC
     transformationMatix2 = np.full((image_h, image_w, 3), transferColor2, dtype='uint8')
 
     # Masking (extracting pixels of the color range from the img)
-    if lower_color_bounds1[0]>upper_color_bounds1[0] :
+    if Red1:
         mask1 = cv2.inRange(frame_HSV, 0, upper_color_bounds1)  # Detect an object based on the range of pixel values in the HSV colorspace.
         mask_rgb1 = cv2.cvtColor(mask1, cv2.COLOR_GRAY2BGR)
         mask12 = cv2.inRange(frame_HSV, lower_color_bounds1, 180)  # Detect an object based on the range of pixel values in the HSV colorspace.
@@ -27,7 +33,7 @@ def ChangeTshirtColors (img, lower_color_bounds1, upper_color_bounds1, transferC
     else :
         mask1 = cv2.inRange(frame_HSV, lower_color_bounds1, upper_color_bounds1)  # Detect an object based on the range of pixel values in the HSV colorspace.
         mask_rgb1 = cv2.cvtColor(mask1, cv2.COLOR_GRAY2BGR)
-    if lower_color_bounds2[0]>upper_color_bounds2[0] :
+    if Red2 :
         mask2 = cv2.inRange(frame_HSV, 0, upper_color_bounds2)  # Detect an object based on the range of pixel values in the HSV colorspace.
         mask_rgb2 = cv2.cvtColor(mask2, cv2.COLOR_GRAY2BGR)
         mask21 = cv2.inRange(frame_HSV, lower_color_bounds1, 180)  # Detect an object based on the range of pixel values in the HSV colorspace.
@@ -40,9 +46,9 @@ def ChangeTshirtColors (img, lower_color_bounds1, upper_color_bounds1, transferC
     # cv2.imshow("Mssk RGB  ", mask_rgb)
     maskedFrame1 = frame & mask_rgb1
     maskedFrame2 = frame & mask_rgb2
-    if lower_color_bounds1[0]>upper_color_bounds1[0] :
+    if Red1:
         maskedFrame12 = frame & mask_rgb12
-    if lower_color_bounds2[0]>upper_color_bounds2[0] :
+    if Red2 :
         maskedFrame21 = frame & mask_rgb21   
         cv2.imshow("maskedFrame2",maskedFrame2)
 
@@ -50,15 +56,15 @@ def ChangeTshirtColors (img, lower_color_bounds1, upper_color_bounds1, transferC
     # Thresholding the mask to get mask of all 1s
     ret1, thresholdedMask1 = cv2.threshold(maskedFrame1, 0, 255, cv2.THRESH_BINARY)
     ret2, thresholdedMask2 = cv2.threshold(maskedFrame2, 0, 255, cv2.THRESH_BINARY)
-    if lower_color_bounds1[0]>upper_color_bounds1[0] :
+    if Red1:
         ret12, thresholdedMask12 = cv2.threshold(maskedFrame12, 0, 255, cv2.THRESH_BINARY)
-    if lower_color_bounds2[0]>upper_color_bounds2[0] :
+    if Red2 :
         ret21, thresholdedMask21 = cv2.threshold(maskedFrame21, 0, 255, cv2.THRESH_BINARY)    # Creating a mask of the transformation matrix values (will be added to original img to change color)
     maskedTransformationMatrix1 = thresholdedMask1 & transformationMatix1
     maskedTransformationMatrix2 = thresholdedMask2 & transformationMatix2
-    if lower_color_bounds1[0]>upper_color_bounds1[0] :
+    if Red1:
         maskedTransformationMatrix12 = thresholdedMask12 & transformationMatix1
-    if lower_color_bounds2[0]>upper_color_bounds2[0] :
+    if Red2 :
         maskedTransformationMatrix21 = thresholdedMask21 & transformationMatix2    
         cv2.imshow("maskedTransformationMatrix1", maskedTransformationMatrix2)
 
@@ -66,9 +72,9 @@ def ChangeTshirtColors (img, lower_color_bounds1, upper_color_bounds1, transferC
     # Applying color transformation
     newFrame_HSV = frame_HSV + maskedTransformationMatrix1
     newFrame_HSV = newFrame_HSV + maskedTransformationMatrix2
-    if lower_color_bounds1[0]>upper_color_bounds1[0] :
+    if Red1:
         newFrame_HSV = newFrame_HSV + maskedTransformationMatrix12
-    if lower_color_bounds2[0]>upper_color_bounds2[0] :
+    if Red2 :
         newFrame_HSV = newFrame_HSV + maskedTransformationMatrix21    
     return cv2.cvtColor(newFrame_HSV, cv2.COLOR_HSV2BGR)
 
