df2124dac9c37438ab8e67a40a150e3be970ed90
amrabdelhamidhassan <33031176+amrabdelhamidhassan@users.noreply.github.com>
2019-05-02 19:32:15 +0200

    Update VideoProcessing.py

--- OpenCV_Processing/VideoProcessing.py
+++ OpenCV_Processing/VideoProcessing.py
@@ -10,9 +10,21 @@ from OpenCV_Processing.AuxFunctions import calculateChangeColor, detectSuitableF
 
 def VideoProcessing(Color1, Color2, start, end):
 
-    video = cv2.VideoCapture('videos/temp/video1.mp4')
+    video = cv2.VideoCapture('..//videos//temp//video1.mp4')
+    noFrames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = video.get(cv2.CAP_PROP_FPS)
+    VideoTime=noFrames/fps
+    print (noFrames)
+    startFrame=noFrames*start/VideoTime
+    print (startFrame)
+    endFrame=noFrames*end/VideoTime
+    print(endFrame)
+    count=0
     while video.isOpened():
+        count=count+1
         ret, frame = video.read()
+        if count<startFrame :
+            continue
         if ret==True:
             if detectSuitableFrame(frame):
                     goodframe = frame
@@ -21,7 +33,8 @@ def VideoProcessing(Color1, Color2, start, end):
 
     l1, u1, m1, l2, u2, m2 = extractShirtsColors(goodframe)
     frame_height, frame_width, _ = goodframe.shape
-    outvideo = cv2.VideoWriter('..//videos//temp//video2.avi',cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
+    outvideo = cv2.VideoWriter('..//videos//temp//video2.avi',cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (frame_width, frame_height))
+    video1 = cv2.VideoCapture('..//videos//temp//video1.mp4')
 
     # if the 2 detected ranges are the same, we will work on only one range
     if l1[0] == l2[0] and u1[0] == u2[0]:
@@ -31,8 +44,11 @@ def VideoProcessing(Color1, Color2, start, end):
     # choosing the 2 new colors for the teams
     color1 = calculateChangeColor(Color1, m1)
     color2 = calculateChangeColor(Color2, m2)     
-    while video.isOpened():
-        ret, frame = video.read()
+    while video1.isOpened():
+        count=count+1
+        ret, frame = video1.read()
+        if count >= endFrame:
+            break
         if ret:
             recoloredFrame = ChangeTshirtColors(frame, l1, u1, color1, l2, u2, color2)
             outvideo.write(recoloredFrame)
@@ -41,6 +57,7 @@ def VideoProcessing(Color1, Color2, start, end):
         # cv2.imshow('frame',frame)
     video.release()
     cv2.destroyAllWindows()
-
+def main():
+    VideoProcessing([150,40,40], [80,40,40], 0, 10)
 if __name__ == "__main__":
-    main()  
+    main()
